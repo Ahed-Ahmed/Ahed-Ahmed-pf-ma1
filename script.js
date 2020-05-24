ebfab8981e3234dc5a898fb7b7613ed1ab8fc518
Ahed-Ahmed <63970476+Ahed-Ahmed@users.noreply.github.com>
2020-05-24 21:12:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// question 1
+
+var firstVar;
+
+// question 2
+
+var name = "Ahed";
+
+//question 3
+
+var number = 7;
+
+//question 4
+
+var twenty = 20,
+    five = 5,
+    division = twenty / five;
+
+//question 5
+
+var frog = true;
+
+//question 6
+
+var orderHasShipped = true;
+
+//question 7
+
+if (orderHasShipped === true) {
+    console.log("true");
+} else {
+    console.log("false")
+}
+
+//question 8
+
+for (var i = 0; i < 10; i += 1) {
+    console.log(i);
+}
+
